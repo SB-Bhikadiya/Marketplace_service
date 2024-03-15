a42a42a647ea385b32ab730d8fc4e4f1b4cb8ca1
Dhruvin Beladiya <dhruvinbeladiya01@gmail.com>
2024-03-15 15:18:30 +0530

    POPULATE METADATA

--- controller/nftController.js
+++ controller/nftController.js
@@ -2,18 +2,50 @@ const { NftModel } = require("../model");
 
 exports.getNFTs = async(req,res) => {
     try {
-        if (req.param.id != null) {
+        const page = parseInt(req.query.page)
+        const limit = 16
+
+        const startIndex = (page -  1) * limit
+        const endIndex = page * limit
+
+        if (req.query.id != null) {
             
-        const NFT = await NftModel.findOne({id:req.param.id})
+        const NFT = await NftModel.findOne({id:req.query.id})
         .populate('bids')
         .populate('history')
-        .populate('author');
-        res.send(NFT);
+        .populate('author')
+        .populate('metadata');
+        return res.send(NFT);
         }
-        const NFTs = await NftModel.find()
+        const NFTs = await NftModel.find().skip(startIndex).limit(limit)
             .populate('bids')
             .populate('history')
-            .populate('author');
+            .populate('author')
+            .populate('metadata');
+
+        // // Pagination result
+        // const pagination = {
+        //     currentPage: page,
+        //     totalPages: totalPages
+        // };
+
+        // // If there's a next page
+        // if (endIndex < totalItems) {
+        //     pagination.next = {
+        //         page: page + 1,
+        //         limit: limit
+        //     };
+        // }
+
+        // // If there's a previous page
+        // if (startIndex > 0) {
+        //     pagination.prev = {
+        //         page: page - 1,
+        //         limit: limit
+        //     };
+        // }
+
+        // res.json({ pagination, data: NFTs });
         res.send(NFTs);
     } catch (error) {
         res.send({error: error});
